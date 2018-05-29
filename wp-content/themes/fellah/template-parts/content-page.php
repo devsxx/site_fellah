7ff787ff0cebafc5d063be626386b3826c02362c
Oussama Elmaaroufy <slashnpro@gmail.com>
2018-05-29 16:13:00 +0000

    Update 29-05-2018

--- wp-content/themes/fellah/template-parts/content-page.php
+++ wp-content/themes/fellah/template-parts/content-page.php
@@ -12,7 +12,7 @@
  
 <div class="container">
 	<div class="row">
-		<div class="offset-md-1 col-md-10">
+		<div class="col-md-12">
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<!-- <header class="entry-header">
 					</header> -->
