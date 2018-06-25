21278962d428203709858a6c151d3d7dd2e6b771
Oussama Elmaaroufy <slashnpro@gmail.com>
2018-06-25 17:45:41 +0100

    05-06-2018

--- wp-content/themes/fellah/wpadverts/single.php
+++ wp-content/themes/fellah/wpadverts/single.php
@@ -228,11 +228,31 @@ if (!empty( $next_post ) || !empty( $prev_post )): ?>
                         <?php $image = adverts_get_main_image( get_the_ID() ) ?>
                         
                         <div class="annoce">
-                                <?php if($image): ?>
-                                    <img src="<?php echo esc_attr($image) ?>" alt="" class="advert-item-grow" />
-                                <?php endif; ?>
-                            <div class="hover"></div>
-                        </div>
+										<?php if($image): ?>
+											<img src="<?php echo esc_attr($image) ?>" alt="" class="advert-item-grow" />
+										<?php else: 
+
+											// $terms = get_the_terms( get_the_ID(), 'advert_category' );
+											// if ($terms ){
+											// 	foreach ( $terms as $term ) { 
+											// 		$color = get_field('bg_color', $term);
+											// 		$image = get_field('image', $term);
+											// 		if($term->parent == 0){
+												 ?>
+												 <img src="<?php bloginfo( 'template_url' ) ?>/img/ads.jpg" alt="">
+
+											 			<!-- <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>"> -->
+											 			<!-- <div class="hover_2" style="background-color: <?php echo $color; ?>;"></div> -->
+											 			<?php 
+											// 			break; 
+											// 		}
+											// 	} 
+											// } 
+											?>
+
+										<?php endif; ?>
+									<div class="hover"></div>
+								</div>
 
                         <div class="info_container">
                                 <div class="info">
